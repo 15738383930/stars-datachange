713e8bd9bbcdc495dbb22301470c73d96119a61b
zhouhao <15738383930@163.com>
2021-09-10 17:11:54 +0800

    功能完善。

--- src/main/java/com/stars/datachange/config/StarsMyBatisConfig.java
+++ src/main/java/com/stars/datachange/config/StarsMyBatisConfig.java
@@ -1,17 +1,12 @@
 package com.stars.datachange.config;
 
 import org.apache.ibatis.session.Configuration;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-
-import javax.sql.DataSource;
 
 /**
  * MyBatis配置
@@ -29,16 +24,6 @@ public class StarsMyBatisConfig {
         Configuration configuration = new Configuration();
         configuration.setMapUnderscoreToCamelCase(true);
         return configuration;
-    }
-
-    @Bean
-    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
-        sqlSessionFactoryBean.setDataSource(dataSource);
-        sqlSessionFactoryBean.setConfiguration(configuration());
-        sqlSessionFactoryBean.setTypeAliasesPackage("com.stars.datachange.model");
-        sqlSessionFactoryBean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/*.xml"));
-        return sqlSessionFactoryBean.getObject();
     }
 
 }
