d79b97a55037ae84f5084e2154e683d64c5c2147
shs-zhcssybzj <shs-zhcssybzj@chinaunicom.cn>
2024-08-20 11:10:54 +0800

    banner优化。

--- src/main/java/com/stars/datachange/config/AutoRegistrationConfig.java
+++ src/main/java/com/stars/datachange/config/AutoRegistrationConfig.java
@@ -24,14 +24,22 @@ public class AutoRegistrationConfig {
         if (StarsProperties.config.isBanner()) {
             // logo
             String logo = "stars-datachange-banner.txt";
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new ClassPathResource(logo).getInputStream()))) {
-                reader.lines().forEach(System.out::println);
-            }
+            String logoSimple = "stars-datachange-banner-simple.txt";
             // version
             String path = "META-INF/maven/com.gitee.xuan_zheng/stars-datachange/pom.properties";
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new ClassPathResource(path).getInputStream()))) {
-                String version = reader.lines().filter(o -> o.contains("version")).map(o -> o.split("=")[1]).findFirst().orElse("");
-                System.out.println(String.format("                                           %s\n", version));
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new ClassPathResource(logo).getInputStream()));
+                 BufferedReader logoReader = new BufferedReader(new InputStreamReader(new ClassPathResource(logo).getInputStream()));
+                 BufferedReader logoSimpleReader = new BufferedReader(new InputStreamReader(new ClassPathResource(logoSimple).getInputStream()));
+                 BufferedReader versionReader = new BufferedReader(new InputStreamReader(new ClassPathResource(path).getInputStream()))) {
+                final long count = reader.lines().filter(o -> o.contains("⣠")).count();
+                String version = versionReader.lines().filter(o -> o.contains("version")).map(o -> o.split("=")[1]).findFirst().orElse("");
+                if (count > 0) {
+                    logoReader.lines().forEach(System.out::println);
+                    System.out.println(String.format("                         %s\n", version));
+                } else {
+                    logoSimpleReader.lines().forEach(System.out::println);
+                    System.out.println(String.format("                                           %s\n", version));
+                }
             }
         }
     }
